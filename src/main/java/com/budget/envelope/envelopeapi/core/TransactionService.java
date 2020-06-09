8af567b1e2bfe7cf2543c478af78c742c688b15e
Josh <surettej@bu.edu>
2020-06-08 21:56:44 -0400

    import fix

--- src/main/java/com/budget/envelope/envelopeapi/core/TransactionService.java
+++ src/main/java/com/budget/envelope/envelopeapi/core/TransactionService.java
@@ -1,6 +1,5 @@
 package com.budget.envelope.envelopeapi.core;
 
-import javafx.util.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
